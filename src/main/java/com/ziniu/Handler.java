77de13522bf6d066bc5e8bc8814a6660fd0776db
Ziniu <guo.ziniu.1003@gmail.com>
2023-04-09 17:54:24 +0800

    feat: sample project for spring integration

---
+++ src/main/java/com/ziniu/Handler.java
@@ -0,0 +1,11 @@
+package com.ziniu;
+
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Handler {
+    public void handleMessage(Message<Object> message) {
+        System.out.println("here is the message: " + message.getPayload());
+    }
+}
---
+++ src/main/java/com/ziniu/TestThread.java
@@ -0,0 +1,17 @@
+package com.ziniu;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestThread extends Thread {
+    @Autowired
+    MessageChannel helloChannel;
+    @Override
+    public void run() {
+        helloChannel.send(new GenericMessage<>("this is hello world payload"));
+    }
+
+}
